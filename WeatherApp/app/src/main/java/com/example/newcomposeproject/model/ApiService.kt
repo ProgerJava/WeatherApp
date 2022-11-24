ca75fb757571f894ce3ef64b816bbc0195038c67
Олейников Андрей <110820357+ProgerJava@users.noreply.github.com>
2022-11-24 14:49:07 +0300

    Update ApiService.kt

--- WeatherApp/app/src/main/java/com/example/newcomposeproject/model/ApiService.kt
+++ WeatherApp/app/src/main/java/com/example/newcomposeproject/model/ApiService.kt
@@ -11,13 +11,13 @@ import retrofit2.http.Query
 interface ApiService {
 
 
-    @GET("current.json?key=c2eefb72f61b41e5a1c114053221911&aqi=no")
+    @GET("current.json?key=yourKey&aqi=no")
     fun getQuotes(@Query("q") country: String) : Call<Data>
 
-    @GET("forecast.json?key=c2eefb72f61b41e5a1c114053221911&days=1&aqi=no&alerts=no")
+    @GET("forecast.json?key=yourKey&days=1&aqi=no&alerts=no")
     fun getQuotes_2(@Query("q") country: String) : Call<Data_2>
 
-    @GET("forecast.json?key=c2eefb72f61b41e5a1c114053221911&days=6&aqi=no&alerts=no")
+    @GET("forecast.json?key=yourKey&days=6&aqi=no&alerts=no")
     fun getQuotes_3(@Query("q") country: String) : Call<Data_3>
 
 
